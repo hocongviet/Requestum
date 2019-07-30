33b2e14c9c672271256a37ec4f6308e2a4ea9d83
Vladimir Ho <congviet.kh@gmail.com>
2019-07-30 18:48:14 +0300

    UI, folder hierarchy, useful extensions added.

---
+++ Requestum/Requestum/Application/Modules/RecipeTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeTableViewCell.swift
+//  Requestum
+//
+//  Created by Cong Viet Ho on 7/30/19.
+//  Copyright © 2019 Viety Software. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell, NibLoadable {
+    
+    static let reuseIdentifier = "RecipeTableViewCellIdentifier"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Requestum/Requestum/Application/Modules/RecipeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  RecipeViewController.swift
+//  Requestum
+//
+//  Created by Cong Viet Ho on 7/30/19.
+//  Copyright © 2019 Viety Software. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    @IBOutlet weak var recipeTableView: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+        setSearchBar()
+        setTableView()
+    }
+    
+    
+    private func setNavigationBar() {
+        title = "Recipe labs"
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(hexString: "2D8706")
+
+    }
+    
+    private func setSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    private func setTableView() {
+        recipeTableView.delegate = self
+        recipeTableView.dataSource = self
+        recipeTableView.register(RecipeTableViewCell.nib, forCellReuseIdentifier: RecipeTableViewCell.reuseIdentifier)
+    }
+
+}
+
+extension RecipeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+}
+
+extension RecipeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RecipeTableViewCell.reuseIdentifier, for: indexPath)
+        return cell
+    }
+    
+
+}
