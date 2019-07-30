456bba91709e862d08e1e9482fb77636d305f8bd
Vladimir Ho <congviet.kh@gmail.com>
2019-07-30 17:44:18 +0300

    Empty project with modifications added

---
+++ Requestum/Requestum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Requestum
+//
+//  Created by Cong Viet Ho on 7/30/19.
+//  Copyright © 2019 Viety Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
