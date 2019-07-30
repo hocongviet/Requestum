1ea7b17b41153452d49a50685dd214d385c5ffbd
Vladimir Ho <congviet.kh@gmail.com>
2019-07-31 00:13:23 +0300

    Core Data Stack

---
+++ Requestum/Requestum/Utils/Core Data/RecipeEntity+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeEntity+CoreDataClass.swift
+//  Requestum
+//
+//  Created by Cong Viet Ho on 7/31/19.
+//  Copyright © 2019 Viety Software. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class RecipeEntity: NSManagedObject {
+
+}
